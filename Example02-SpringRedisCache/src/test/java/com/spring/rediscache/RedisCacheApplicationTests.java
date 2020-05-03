43bb34d7fb3b24cc5b091d04ef82ac509e39a2d2
aturk <aturk@netas.com.tr>
2020-05-03 23:55:04 +0300

    added example2

---
+++ Example02-SpringRedisCache/src/test/java/com/spring/rediscache/RedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
