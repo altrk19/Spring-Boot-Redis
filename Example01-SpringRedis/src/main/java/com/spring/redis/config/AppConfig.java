605c44a71b3b2d3817b5a4f085384de853b1d0b2
aturk <aturk@netas.com.tr>
2020-05-03 23:11:55 +0300

    added example1

--- Example01-SpringRedis/src/main/java/com/spring/redis/config/AppConfig.java
+++ Example01-SpringRedis/src/main/java/com/spring/redis/config/AppConfig.java
@@ -2,7 +2,6 @@ package com.spring.redis.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.Jedis;
@@ -10,7 +9,6 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
-@EnableCaching
 public class AppConfig {
     private static final String CONFIG_CONNECTION_PASSWORD = null;
     private static final Integer CONFIG_CONNECTION_PORT = 6379;
